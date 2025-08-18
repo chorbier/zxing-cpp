725ba294862184c779751ef6a024886a688f06e8
Chorbier <dssen@ya.ru>
2025-08-18 17:54:30 +0500

    Correct bottle factor fix

--- core/src/datamatrix/DMDetector.cpp
+++ core/src/datamatrix/DMDetector.cpp
@@ -1187,7 +1187,7 @@ namespace ZXing::DataMatrix {
 	void createMaps(cv::Mat& mapXY, int outputSize, bool horizontal, bool inverse) {
 		mapXY.create(outputSize, outputSize, CV_32FC2);
 
-		float factor = float(outputSize) / 7.6;
+		float factor = float(outputSize) / 7.6 * 0.5;
 
 		static cv::Mat offsetMap;
 
@@ -1560,6 +1560,7 @@ std::array rotateMediun = {
 
 		cv::Mat resizedImg;
 		cv::resize(image.asMat(), resizedImg, {remapSize, remapSize}, 0,0, cv::INTER_LINEAR);
+		cv::threshold(resizedImg, resizedImg, 127, 255, cv::THRESH_BINARY);
 
 		for (int i = 0; i < 4; i++) {
             n1 = 0; n2 = 1;
